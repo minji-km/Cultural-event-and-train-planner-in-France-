fcf9b032a766a855d0a527570ff6fae0aa32d87a
minji-km <minji.kmk@gmail.com>
2024-01-24 19:23:47 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -6,13 +6,6 @@ import datetime
 from streamlit_folium import st_folium
 
 st.title("Planificateur d'évènements culturels")
-grd_villes = ['Paris', 'Avignon', 'Bordeaux', 'Lille', 'Lyon', 'Marseille',
-              'Montpellier', 'Nantes', 'Nice', 'Toulouse']
-
-depart = st.selectbox('Départ', grd_villes, index=grd_villes.index('Paris'))
-arrivee = st.selectbox('Arrivée', grd_villes + ['Toutes'])
-date_depart = st.date_input('Date de départ', min_value=datetime.date.today())
-
 
 @st.cache_data
 def load_data():
