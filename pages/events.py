aa819e25ba5a5f89718c992b70de07e6c2791439
minji-km <minji.kmk@gmail.com>
2024-01-24 19:23:53 +0100

    Update events.py

--- pages/events.py
+++ pages/events.py
@@ -1,4 +1,14 @@
 import streamlit as st
 from Home import load_data
+import datetime
+
+grd_villes = ['Paris', 'Avignon', 'Bordeaux', 'Lille', 'Lyon', 'Marseille',
+              'Montpellier', 'Nantes', 'Nice', 'Toulouse']
+
+depart = st.selectbox('Départ', grd_villes, index=grd_villes.index('Paris'))
+arrivee = st.selectbox('Arrivée', grd_villes + ['Toutes'])
+date_depart = st.date_input('Date de départ', min_value=datetime.date.today())
 
 df = load_data()
+
+tab1, tab2 = st.tabs(grd_villes)
