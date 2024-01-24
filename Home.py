da62e97f8a87007ec02f22a3b5a2aaedc713fac7
minji-km <minji.kmk@gmail.com>
2024-01-24 19:01:01 +0100

    Update Home.py

---
+++ Home.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+import trainline
+import folium
+from streamlit_folium import st_folium
+
+st.title("Planificateur d'évènements culturels")
+df = pd.read_csv("concerts.csv")
+
+latitude_moyenne = df['latitude'].mean()
+longitude_moyenne = df['longitude'].mean()
+carte = folium.Map(location=[latitude_moyenne, longitude_moyenne], zoom_start=7)
+
+for idx, row in df.iterrows():
+    folium.Marker(
+        location=[row['latitude'], row['longitude']],
+        popup=row['titre'],
+        icon=folium.Icon(icon="cloud"),
+    ).add_to(carte)
+
+st_data = st_folium(carte, width=723)
