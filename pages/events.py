4ae532689d16d6aec8d251b269bc361087ed4b25
minji-km <minji.kmk@gmail.com>
2024-01-24 19:42:21 +0100

    Update events.py

--- pages/events.py
+++ pages/events.py
@@ -9,7 +9,7 @@ grd_villes = ['Paris', 'Avignon', 'Bordeaux', 'Lille', 'Lyon', 'Marseille',
 
 depart = st.selectbox('Départ', grd_villes, index=grd_villes.index('Paris'))
 arrivee = st.selectbox('Arrivée', grd_villes + ['Toutes'])
-date_depart = st.date_input('Date de départ', min_value=datetime.date.today())
+date_depart = st.date_input('Date de départ', min_value=datetime.date.today(), max_value=datetime.date(2024, 12, 31))
 
 df = load_data()
 
