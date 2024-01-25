90eaafa76a6a6d8602e6c939f9ebf9b0984c495f
minji-km <minji.kmk@gmail.com>
2024-01-25 12:09:49 +0100

    Update cultural_events.py

--- cultural_events.py
+++ cultural_events.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+from streamlit_folium import st_folium
+import folium
 
 st.title("Planificateur d'évènements culturels")
 
@@ -12,10 +14,21 @@ def load_data():
     return df
 
 df = load_data()
-df['latitude'] = df['latitude'].astype(float)
-df['longitude'] = df['longitude'].astype(float)
 
-st.map(df[df['result_city']=='Paris'])
+paris_data = df[df['result_city']=='Paris']
+
+latitude_moyenne = paris_data['latitude'].mean()
+longitude_moyenne = paris_data['longitude'].mean()
+carte = folium.Map(location=[latitude_moyenne, longitude_moyenne], zoom_start=12)
+
+for idx, row in df.iterrows():
+    folium.Marker(
+        location=[row['latitude'], row['longitude']],
+        popup=row['titre'],
+        icon=folium.Icon(icon="cloud"),
+    ).add_to(carte)
+
+st_data = st_folium(carte, width=1000)
 
 
 # i = 0
