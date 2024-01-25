88b826ebac078c8e23316095ee7e3182374463ef
minji-km <minji.kmk@gmail.com>
2024-01-25 11:03:26 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -17,34 +17,6 @@ df['longitude'] = df['longitude'].astype(float)
 
 st.map(df[df['result_city']=='Paris'])
 
-tabs = st.tabs(grd_villes)
-
-with tabs[0]:
-    st.map(df[df['result_city']=='Avignon'])
-
-with tabs[1]:
-    st.map(df[df['result_city']=='Bordeaux'])
-
-with tabs[2]:
-    st.map(df[df['result_city']=='Lille'])
-
-with tabs[3]:
-    st.map(df[df['result_city']=='Lyon'])
-    
-with tabs[4]:
-    st.map(df[df['result_city']=='Marseille'])
-
-with tabs[5]:
-    st.map(df[df['result_city']=='Montpellier'])
-
-with tabs[6]:
-    st.map(df[df['result_city']=='Nantes'])
-
-with tabs[7]:
-    st.map(df[df['result_city']=='Nice'])
-
-with tabs[8]:
-    st.map(df[df['result_city']=='Toulouse'])
 
 # i = 0
 # for ville in grd_villes:
