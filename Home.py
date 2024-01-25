05879c0ee4cf3f0c47dc5bc6e59c7331de401017
minji-km <minji.kmk@gmail.com>
2024-01-25 10:59:09 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 st.title("Planificateur d'évènements culturels")
 
-grd_villes = ['Paris', 'Avignon', 'Bordeaux', 'Lille', 'Lyon', 'Marseille',
+grd_villes = ['Avignon', 'Bordeaux', 'Lille', 'Lyon', 'Marseille',
               'Montpellier', 'Nantes', 'Nice', 'Toulouse']
 
 @st.cache_data
@@ -15,37 +15,36 @@ df = load_data()
 df['latitude'] = df['latitude'].astype(float)
 df['longitude'] = df['longitude'].astype(float)
 
+st.map(df[df['result_city']=='Paris'])
+
 tabs = st.tabs(grd_villes)
 
 with tabs[0]:
-    st.map(df[df['result_city']=='Paris'])
+    st.map(df[df['result_city']=='Avignon'])
 
-# with tabs[1]:
-#     st.map(df[df['result_city']=='Avignon'])
+with tabs[1]:
+    st.map(df[df['result_city']=='Bordeaux'])
 
-# with tabs[2]:
-#     st.map(df[df['result_city']=='Bordeaux'])
+with tabs[2]:
+    st.map(df[df['result_city']=='Lille'])
 
-# with tabs[3]:
-#     st.map(df[df['result_city']=='Lille'])
-
-# with tabs[4]:
-#     st.map(df[df['result_city']=='Lyon'])
+with tabs[3]:
+    st.map(df[df['result_city']=='Lyon'])
     
-# with tabs[5]:
-#     st.map(df[df['result_city']=='Marseille'])
+with tabs[4]:
+    st.map(df[df['result_city']=='Marseille'])
 
-# with tabs[6]:
-#     st.map(df[df['result_city']=='Montpellier'])
+with tabs[5]:
+    st.map(df[df['result_city']=='Montpellier'])
 
-# with tabs[7]:
-#     st.map(df[df['result_city']=='Nantes'])
+with tabs[6]:
+    st.map(df[df['result_city']=='Nantes'])
 
-# with tabs[8]:
-#     st.map(df[df['result_city']=='Nice'])
+with tabs[7]:
+    st.map(df[df['result_city']=='Nice'])
 
-# with tabs[9]:
-#     st.map(df[df['result_city']=='Toulouse'])
+with tabs[8]:
+    st.map(df[df['result_city']=='Toulouse'])
 
 # i = 0
 # for ville in grd_villes:
