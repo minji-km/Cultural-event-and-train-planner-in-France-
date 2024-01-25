a4439e83829d5c6c210fa55d552e6484bf659764
minji-km <minji.kmk@gmail.com>
2024-01-25 10:58:00 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -20,13 +20,32 @@ tabs = st.tabs(grd_villes)
 with tabs[0]:
     st.map(df[df['result_city']=='Paris'])
 
-with tabs[1]:
-    st.map(df[df['result_city']=='Avignon'])
+# with tabs[1]:
+#     st.map(df[df['result_city']=='Avignon'])
 
-with tabs[2]:
-    st.map(df[df['result_city']=='Bordeaux'])
+# with tabs[2]:
+#     st.map(df[df['result_city']=='Bordeaux'])
 
+# with tabs[3]:
+#     st.map(df[df['result_city']=='Lille'])
 
+# with tabs[4]:
+#     st.map(df[df['result_city']=='Lyon'])
+    
+# with tabs[5]:
+#     st.map(df[df['result_city']=='Marseille'])
+
+# with tabs[6]:
+#     st.map(df[df['result_city']=='Montpellier'])
+
+# with tabs[7]:
+#     st.map(df[df['result_city']=='Nantes'])
+
+# with tabs[8]:
+#     st.map(df[df['result_city']=='Nice'])
+
+# with tabs[9]:
+#     st.map(df[df['result_city']=='Toulouse'])
 
 # i = 0
 # for ville in grd_villes:
