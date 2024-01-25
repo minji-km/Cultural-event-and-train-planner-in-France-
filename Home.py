2d3c340c43aa60891878316f884018ba31eb8ffd
minji-km <minji.kmk@gmail.com>
2024-01-25 10:25:31 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,14 +1,9 @@
 import streamlit as st
 import pandas as pd
-import trainline
-import folium
-from streamlit_folium import st_folium
-import ipywidgets as widgets
-from IPython.display import display
 
 st.title("Planificateur d'évènements culturels")
 
-@st.cache_data
+@st.cache
 def load_data():
     df = pd.read_csv("concerts.csv")
     return df
@@ -28,15 +23,6 @@ coords = {
     'Toulouse': [43.6043, 1.4437]
 }
 
-latitude_moyenne = df['latitude'].mean()
-longitude_moyenne = df['longitude'].mean()
-carte = folium.Map(location=[latitude_moyenne, longitude_moyenne], zoom_start=7)
-
-for idx, row in df.iterrows():
-    folium.Marker(
-        location=[row['latitude'], row['longitude']],
-        popup=row['titre'],
-        icon=folium.Icon(icon="cloud"),
-    ).add_to(carte)
-
-st_data = st_folium(carte, width=1000)
+ville = st.selectbox('Choisissez une ville', list(coords.keys()))
+df_ville = pd.DataFrame([coords[ville]], columns=['lat', 'lon'])
+st.map(df_ville)
